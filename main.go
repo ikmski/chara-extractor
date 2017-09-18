2a1ee5cfeb7152e3e2657cb8b0dd68613343df80
masaki <ikeda.masaki@gmail.com>
2017-09-18 15:18:55 +0900

    split functions

--- main.go
+++ main.go
@@ -8,29 +8,24 @@ import (
 	"github.com/urfave/cli"
 )
 
-func mainAction(c *cli.Context) error {
+func extract(text []string) []rune {
 
-	if c.NArg() == 0 {
-		return fmt.Errorf("input is required")
-	}
-	txt := c.Args()
+	m := make(map[rune]rune)
 
-	unique := make(map[rune]rune)
+	for _, line := range text {
 
-	for _, line := range txt {
 		chars := []rune(line)
-
 		for _, char := range chars {
 
-			_, ok := unique[char]
+			_, ok := m[char]
 			if !ok {
-				unique[char] = char
+				m[char] = char
 			}
 		}
 	}
 
 	runes := []rune{}
-	for key, _ := range unique {
+	for key, _ := range m {
 		runes = append(runes, key)
 	}
 
@@ -38,8 +33,36 @@ func mainAction(c *cli.Context) error {
 		return runes[i] < runes[j]
 	})
 
+	return runes
+}
+
+func outputChar(runes []rune) {
+
 	for _, r := range runes {
 		fmt.Printf("%v", string(r))
+	}
+	fmt.Print("\n")
+}
+
+func outputDecimal(runes []rune) {
+
+	for i := 0; i < len(runes)-1; i++ {
+		fmt.Printf("%d,", runes[i])
+	}
+	fmt.Printf("%d\n", runes[len(runes)-1])
+}
+
+func mainAction(c *cli.Context) error {
+
+	if c.NArg() == 0 {
+		return fmt.Errorf("input is required")
+	}
+	text := c.Args()
+
+	if c.Bool("d") {
+		outputDecimal(extract(text))
+	} else {
+		outputChar(extract(text))
 	}
 
 	return nil
@@ -50,6 +73,13 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "chara-extractor"
 	app.Usage = "Character Extractor"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "decimal, d",
+			Usage: "output decimal number?",
+		},
+	}
 
 	app.Action = mainAction
 
