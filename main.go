6793002381974dcf7bd5061b116f76fd0fb90d45
masaki <ikeda.masaki@gmail.com>
2017-09-18 11:15:15 +0900

    add impliment

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"sort"
 
 	"github.com/urfave/cli"
 )
 
 func mainAction(c *cli.Context) error {
+
+	if c.NArg() == 0 {
+		return fmt.Errorf("input is required")
+	}
+	txt := c.Args()
+
+	unique := make(map[rune]rune)
+
+	for _, line := range txt {
+		chars := []rune(line)
+
+		for _, char := range chars {
+
+			_, ok := unique[char]
+			if !ok {
+				unique[char] = char
+			}
+		}
+	}
+
+	runes := []rune{}
+	for key, _ := range unique {
+		runes = append(runes, key)
+	}
+
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+
+	for _, r := range runes {
+		fmt.Printf("%v", string(r))
+	}
 
 	return nil
 }
